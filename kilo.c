59be5e62b9c813d356881dba38b0b865a0f17084
radwtsm <radwtsm@gmail.com>
2024-02-21 07:26:17 +0100

    Software flow control events turned off, turned off output processing, miscellaneous flags disabled for technical raw mode

--- kilo.c
+++ kilo.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -13,7 +15,6 @@ void disableRawMode(void)
 
 void enableRawMode(void)
 {
-  struct termios raw;
   // get settings from terminal and saves it in "raw"
   tcgetattr(STDIN_FILENO, &orig_termios);
 
@@ -24,8 +25,26 @@ void enableRawMode(void)
   struct termios raw = orig_termios;
 
   // avoid showing user input in terminal
+  // c_lflag stands for local flags , when flags start with "I" it mean they are input flags, event tho ICANON and ISIG are not input flags.
   // bitwise AND with the NOT(ECHO), we switch the ECHO attribute
-  raw.c_lflag &= ~(ECHO);
+  // With ICANON we can now read input byte by byte instead of lines.
+  // With ISIG we disable the SIGINT (Ctrl-c) and the SIGTSTP(Ctrl-z)
+  // With IEXTEN we disable Ctrl-V (i.e if you type Ctrl-C Ctrl-V we could input 3 bytes)
+  // By default, Ctrl-S and Ctrl-Q are used for software flow control. Ctrl-S stops data from being transmitted to the terminal until you press Ctrl-Q.
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  // IXON turns off XON (Ctrl-S to pause transmission) and XOFF (Ctrl-Q to resume) events.
+  // ICRNL transaltes any carriage returns into newlines.
+  // BRKINT sends will sedn a SIGINT signal when a break condition occurs
+  // INPCK enables parity checking, which doesn’t seem to apply to modern terminal emulators.
+  // ISTRIP causes the 8th bit of each input byte to be stripped, meaning it will set it to 0. This is probably already turned off.
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+
+  // OPOST outpuf flag, disables all output processing
+  // by default every "\n" is converted to "\r\n"
+  raw.c_oflag &= ~(OPOST);
+
+  // CS8 is not a flag, it is a bit mask with multiple bits, which we set using the bitwise-OR (|) operator unlike all the flags we are turning off. It sets the character size (CS) to 8 bits per byte. On my system, it’s already set that way.
+  raw.c_cflag |= (CS8);
 
   // we use our struct for setting the attributes in the terminal
   //  TCSAFLUSH, the change shall occur after all output written to fildes is transmitted, and all input so far received but not read shall be discarded before the change is made.
@@ -42,7 +61,19 @@ int main(void)
   // we use read() to read a 1 byte from STDIN_FILENO (or standard input)
   // q keypress is excludes, so we can use the "q" to quit
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
-    ;
+  {
+    // checks for control characters (characters that are not printable i.e Escape)
+    if (iscntrl(c))
+    {
+      // we manually add \r to move the cursor left (we disabled OPOST flag that prepends a \r to \n)
+      printf("%d \r\n", c);
+    }
+    else
+    {
+      // we show the char as a decimal number (%d) and as a byte directly (%c)
+      printf("%d %c \r\n", c, c);
+    }
+  }
 
   return 0;
 }
